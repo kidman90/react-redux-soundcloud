6a8d2c670832860000b43dd7757cbf2a6b86846c
kidman90 <kacprzak90@gmail.com>
2018-06-04 14:41:35 +0200

    The Minimal React + Webpack 4 + Babel Setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.querySelector('#app')
+);
+
+module.hot.accept();
